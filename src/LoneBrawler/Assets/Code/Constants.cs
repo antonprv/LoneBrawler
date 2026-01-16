c9396f57fea14ea34340337d7a634860c777ebd2
Your Name <anton.piruev.97@yandex.com>
2026-01-16 18:23:50 +0400

    Initialized basic code

---
+++ src/LoneBrawler/Assets/Code/Constants.cs
@@ -0,0 +1,9 @@
+// Created by Anton Piruev in 2025. Any direct commercial use of derivative work is strictly prohibited.
+
+namespace CodeBase
+{
+  public static class Constants
+  {
+    public const float Epsilon = 0.001f;
+  }
+}
